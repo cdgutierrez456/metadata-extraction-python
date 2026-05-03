de89a23ca8bdf458fa68aeb87033d5226138ce16
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-05-03 14:17:16 -0500

    Creando factoria inicial para extraccion de metadata

---
+++ main.py
@@ -0,0 +1,7 @@
+from metadata_analyzer import extract_metadata
+
+if __name__ == '__main__':
+    filepath = 'jolla.jpg'
+    metadata = extract_metadata(filepath)
+    for key, value in metadata.items():
+        print(f'{key}: {value}')
